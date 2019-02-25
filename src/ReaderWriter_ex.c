9f7fe8895ce2a5d75c11388ae19472807e8952de
d3nisdantas <d3nisdantas@gmail.com>
2019-02-25 11:29:59 +0100

    Added multiple threads to read and write. Also changed the shared variable to an int instead of an array

--- src/ReaderWriter_ex.c
+++ src/ReaderWriter_ex.c
@@ -11,57 +11,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define BUF_SIZE 3 /* size of shared buffer */
+#define EXEC_NUM 5 /* number of times the threads will execute */
 
-int buffer[BUF_SIZE];
+//int buffer[BUF_SIZE];
+int sharedVar;
 int add = 0;	/* place to add the next element */
 int rem = 0;	/* place to remove the next element */
 int num = 0;	/* number of elements in a buffer */
+int readers = 0;
+int writers = 0;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;		/* mutex for buffer */
 pthread_cond_t c_cons = PTHREAD_COND_INITIALIZER;	/* consumer waits on cv */
 pthread_cond_t c_prod = PTHREAD_COND_INITIALIZER;
 
-void *producer(void *params);
-void *consumer(void *params);
+static void *producer(void *params);
+static void *consumer(void *params);
 
 int main(void) {
-	pthread_t tid1, tid2;
-	int i;
+	pthread_t myThreads[10];
+	int *args[5];
 
-	if (pthread_create(&tid1, NULL, producer, NULL) !=0){
-		fprintf(stderr, "Unable to create producer thread.\n");
-		exit(1);
+	for(int i = 0; i < 5; i++)
+	{
+		//create writers
+		args[i] = (int*)malloc(sizeof(int));
+		if(args[i] != NULL)
+			*args[i] = i;
+
+		if (pthread_create(&myThreads[i], NULL, producer, args[i]) !=0)
+		{
+			fprintf(stderr, "Unable to create producer thread.\n");
+			exit(1);
+		}
 	}
 
-	if (pthread_create(&tid2, NULL, consumer, NULL) !=0){
-		fprintf(stderr, "Unable to create consumer thread.\n");
-		exit(1);
+	for(int i = 5; i < 10; i++)
+	{
+		//create readers
+		if (pthread_create(&myThreads[i], NULL, consumer, NULL) !=0)
+		{
+			fprintf(stderr, "Unable to create consumer thread.\n");
+			exit(1);
+		}
 	}
 
-	pthread_join(tid1, NULL);
-	pthread_join(tid2, NULL);
+	for(int i = 0; i < 10; i++)
+	{
+		pthread_join(myThreads[i], NULL);
+	}
+	for(int i = 0; i < 5; i++)
+	{
+		free(args[i]);
+	}
 
 	printf("parent quitting.\n");
+	return 0;
 }
 
-void *producer(void *parms){
-	int i;
-	for(i = 1; i <= 20; i++){
+void *producer(void *params){
+	int myNumber = *((int*)params);
+	int startNumber = EXEC_NUM*myNumber;
+
+	for(int i = startNumber; i < startNumber + EXEC_NUM; i++){
 		pthread_mutex_lock(&m);
-			if(num > BUF_SIZE){ //overflow
-				exit(1);
-			}
-			while(num == BUF_SIZE){
+			while(writers || readers){
+				// a writer can only write when there are no other writers or readers executing
 				pthread_cond_wait(&c_prod, &m);
 			}
-			buffer[add] = i; // buffer not full, so add element
-			add = (add+1) % BUF_SIZE;
-			num++;
+			writers++;
+			sleep(1);
+			sharedVar = i;
+			writers--;
 		pthread_mutex_unlock(&m);
-		pthread_cond_signal(&c_cons);
-		printf("producer inserted %d\n", i); fflush(stdout);
+		pthread_cond_broadcast(&c_cons);
+
+		printf("Producer %lu inserted %d\n", pthread_self(), i); fflush(stdout);
+		printf("Num of readers %d \n", readers); fflush(stdout);
 	}
 
 	printf("producer quitting\n"); fflush(stdout);
@@ -69,22 +98,28 @@ void *producer(void *parms){
 }
 
 void *consumer(void *params){
-	int i;
+	int readValue = -1;
 
-	while(1){
+	for(int j = 0; j < EXEC_NUM; j++){
 		pthread_mutex_lock(&m);
 			if (num < 0){ //underflow
 				exit(1);
 			}
-			while(num == 0){
+			while(writers){
 				pthread_cond_wait(&c_cons, &m);
 			}
-			i = buffer[rem]; // buffer not empty, so remove element
-			rem = (rem+1) % BUF_SIZE;
-			num --;
+			readers++;
+			sleep(1);
+			readValue = sharedVar;
+			readers--;
 		pthread_mutex_unlock(&m);
 
-		pthread_cond_signal(&c_prod);
-		printf("Consume value %d\n", i); fflush(stdout);
+		if(writers == 0)
+			pthread_cond_signal(&c_prod);
+
+		printf("Consumer %lu read %d\n", pthread_self(), readValue); fflush(stdout);
+		printf("Num of readers %d \n", readers); fflush(stdout);
 	}
+
+	return 0;
 }
