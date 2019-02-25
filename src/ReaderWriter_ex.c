c18e602024a5e411b1d91fcb780cccfe408f1b50
d3nisdantas <d3nisdantas@gmail.com>
2019-02-25 16:56:42 +0100

    Update to explicit create critical session using mutex/signal

--- src/ReaderWriter_ex.c
+++ src/ReaderWriter_ex.c
@@ -15,110 +15,132 @@
 
 #define BUF_SIZE 3 /* size of shared buffer */
 #define EXEC_NUM 5 /* number of times the threads will execute */
+#define READ_NUM 5
+#define WRITE_NUM 5
 
 //int buffer[BUF_SIZE];
 int sharedVar;
 int add = 0;	/* place to add the next element */
 int rem = 0;	/* place to remove the next element */
 int num = 0;	/* number of elements in a buffer */
-int readers = 0;
-int writers = 0;
+int gReaders = 0;
+int gWaitingReaders = 0;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;		/* mutex for buffer */
-pthread_cond_t c_cons = PTHREAD_COND_INITIALIZER;	/* consumer waits on cv */
-pthread_cond_t c_prod = PTHREAD_COND_INITIALIZER;
+pthread_cond_t readPhase = PTHREAD_COND_INITIALIZER;	/* consumer waits on cv */
+pthread_cond_t writePhase = PTHREAD_COND_INITIALIZER;
 
 static void *producer(void *params);
 static void *consumer(void *params);
 
 int main(void) {
-	pthread_t myThreads[10];
-	int *args[5];
+	int readIds[READ_NUM];
+	int writeIds[WRITE_NUM];
+	pthread_t readerThreads[READ_NUM];
+	pthread_t writerThreads[WRITE_NUM];
 
-	for(int i = 0; i < 5; i++)
+	for(int i = 0; i < WRITE_NUM; i++)
 	{
 		//create writers
-		args[i] = (int*)malloc(sizeof(int));
-		if(args[i] != NULL)
-			*args[i] = i;
+		writeIds[i] = i;
 
-		if (pthread_create(&myThreads[i], NULL, producer, args[i]) !=0)
+		if (pthread_create(&writerThreads[i], NULL, producer, &writeIds[i]) !=0)
 		{
 			fprintf(stderr, "Unable to create producer thread.\n");
 			exit(1);
 		}
 	}
 
-	for(int i = 5; i < 10; i++)
+	for(int i = 0; i < READ_NUM; i++)
 	{
 		//create readers
-		if (pthread_create(&myThreads[i], NULL, consumer, NULL) !=0)
+		readIds[i] = i;
+
+		if (pthread_create(&readerThreads[i], NULL, consumer, &readIds[i]) !=0)
 		{
 			fprintf(stderr, "Unable to create consumer thread.\n");
 			exit(1);
 		}
 	}
 
-	for(int i = 0; i < 10; i++)
+	for(int i = 0; i < READ_NUM; i++)
 	{
-		pthread_join(myThreads[i], NULL);
+		pthread_join(readerThreads[i], NULL);
 	}
-	for(int i = 0; i < 5; i++)
+	for(int i = 0; i < WRITE_NUM; i++)
 	{
-		free(args[i]);
+		pthread_join(writerThreads[i], NULL);
 	}
 
 	printf("parent quitting.\n");
 	return 0;
 }
 
-void *producer(void *params){
-	int myNumber = *((int*)params);
-	int startNumber = EXEC_NUM*myNumber;
+static void *producer(void *params){
+	int id = *((int*)params);
+	int numReaders = 0;
 
-	for(int i = startNumber; i < startNumber + EXEC_NUM; i++){
+	for(int i = 0; i < EXEC_NUM; i++)
+	{
+		// Wait so that all threads don't take the lock at the same time
+		usleep(1000 * (random() % 5 + 5));
+		// Enter critical session
 		pthread_mutex_lock(&m);
-			while(writers || readers){
+			while(gReaders)
+			{
 				// a writer can only write when there are no other writers or readers executing
-				pthread_cond_wait(&c_prod, &m);
+				pthread_cond_wait(&writePhase, &m);
 			}
-			writers++;
-			sleep(1);
-			sharedVar = i;
-			writers--;
+			gReaders = -1;
+			numReaders = gReaders;
 		pthread_mutex_unlock(&m);
-		pthread_cond_broadcast(&c_cons);
 
-		printf("Producer %lu inserted %d\n", pthread_self(), i); fflush(stdout);
-		printf("Num of readers %d \n", readers); fflush(stdout);
+		printf("Producer %lu writing %d. [Readers: %d]\n", id, ++sharedVar, numReaders); fflush(stdout);
+
+		// Exit critical session
+		pthread_mutex_lock(&m);
+			gReaders = 0;
+			if(gWaitingReaders > 0)
+				pthread_cond_broadcast(&readPhase);
+			else
+				pthread_cond_signal(&writePhase);
+
+		pthread_mutex_unlock(&m);
 	}
 
-	printf("producer quitting\n"); fflush(stdout);
+	printf("producer %d, quitting\n", id); fflush(stdout);
 	return 0;
 }
 
-void *consumer(void *params){
-	int readValue = -1;
+static void *consumer(void *params)
+{
+	int id = *((int*)params);
+	int numReaders = 0;
 
-	for(int j = 0; j < EXEC_NUM; j++){
+	for(int j = 0; j < EXEC_NUM; j++)
+	{
+		// Wait so that all threads don't take the lock at the same time
+		usleep(1000 * (random() % 5 + 5));
+		// Enter critical session
 		pthread_mutex_lock(&m);
-			if (num < 0){ //underflow
-				exit(1);
+			gWaitingReaders++;
+			while(gReaders == -1)
+			{
+				pthread_cond_wait(&readPhase, &m);
 			}
-			while(writers){
-				pthread_cond_wait(&c_cons, &m);
-			}
-			readers++;
-			sleep(1);
-			readValue = sharedVar;
-			readers--;
+			gWaitingReaders--;
+			numReaders = ++gReaders;
 		pthread_mutex_unlock(&m);
 
-		if(writers == 0)
-			pthread_cond_signal(&c_prod);
+		printf("Consumer %lu read %d. [Readers: %d]\n", id, sharedVar, numReaders); fflush(stdout);
 
-		printf("Consumer %lu read %d\n", pthread_self(), readValue); fflush(stdout);
-		printf("Num of readers %d \n", readers); fflush(stdout);
+		//Exit critical session
+		pthread_mutex_lock(&m);
+			gReaders--;
+			if(gReaders == 0)
+				pthread_cond_signal(&writePhase);
+
+		pthread_mutex_unlock(&m);
 	}
 
 	return 0;
